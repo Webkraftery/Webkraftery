5defd3808fc14cf3aefe3f38ff757b94da364c31
Kunal Koushik <kunalkoushik44@gmail.com>
2025-06-29 19:40:39 +0530

    Portfolio data

---
+++ src/data/mockProjects.js
@@ -0,0 +1,19 @@
+export const mockProjects = [
+  {
+    id: 1,
+    title: "PlanetCode Edtech Application",
+    description: "Planet Code is an interactive edtech platform where learners explore coding through gamified missions, projects, and hands-on challenges.",
+    techStack: ["React", "Node.js", "MongoDB", "ExpressJS", "Cloudinary"],
+    date: "2025-04",
+    teamSize: 1,
+    thumbnail: "src/assets/portfolio-Images/planetcode1.png",
+    backgroundImage: ["src/assets/portfolio-Images/planetcode1.png","src/assets/portfolio-Images/planetcode2.png"],
+    demoLink: "https://planetcode.vercel.app",
+    githubLink: "https://github.com/company/ecommerce-platform",
+    detailedDescription: "Planet Code is a dynamic edtech platform designed to make coding fun and engaging for students of all ages. Through interactive lessons, gamified challenges, and real-world projects, it helps learners build strong programming foundations while exploring the creative side of technology.",
+    achievements: ["🚀 Built an interactive, gamified coding platform.", "📈 Increased learner engagement by 70%.", "Designed adaptive learning paths for all levels.","📚 Created project-based, real-world coding lessons."]
+  },
+  
+  
+  
+];
