d25fe17dc4c3412e562dc63859efef64c3225632
Kunal Koushik <kunalkoushik44@gmail.com>
2025-06-30 15:08:10 +0530

    BG

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="noto-serif bg-gray-800 text-gray-300 py-12 px-4 sm:px-6 lg:px-8 font-sans">
+    <footer className="noto-serif z-50 bg-gray-800 text-gray-300 py-12 px-4 sm:px-6 lg:px-8 font-sans">
       <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10">
 
         {/* Company Info Section */}
